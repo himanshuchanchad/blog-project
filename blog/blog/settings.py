e745d317c53f0b6ee4625a230521b8be73b93233
Himanshu Chanchad <himanshuchanchad@gmail.com>
2019-03-10 15:23:30 +0530

    tried mysql

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -87,7 +87,17 @@ DATABASES = {
     }
 }
 
-
+# DATABASES={
+#     'default':{
+#         'ENGINE':'django.db.backends.mysql',
+#         'NAME':'blog',
+#         'USER':'root',
+#         'PASSWORD':'',
+#         'HOST':'localhost',
+#         'PORT':'3306',
+#
+#     }
+# }
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
 
